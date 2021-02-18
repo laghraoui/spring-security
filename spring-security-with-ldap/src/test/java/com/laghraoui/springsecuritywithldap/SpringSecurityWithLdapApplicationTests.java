1ab0a1b2ec0ebe06698565af12f6a0fb610c3d5d
laghraoui <elmehdi.laghraoui@gmail.com>
2021-02-18 21:58:23 +0100

    spring boot app example with ldap

---
+++ spring-security-with-ldap/src/test/java/com/laghraoui/springsecuritywithldap/SpringSecurityWithLdapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laghraoui.springsecuritywithldap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityWithLdapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
